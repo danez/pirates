59437440caed1b164eaa1cb69351dfcf9bd0f2fd
Ari Porad <git@ariporad.com>
2015-12-06 10:21:12 -0800

    refactor(tests): rename the tests, they are now hooks, not compilers

--- test/fixture/main.js
+++ test/fixture/main.js
@@ -1,9 +1,9 @@
 /* (c) 2015 Ari Porad (@ariporad) <http://ariporad.com>. License: ariporad.mit-license.org */
 
-require('compilerA');
-require('compilerB');
-require('compilerC');
-require('compilerD');
+require('hookA');
+require('hookB');
+require('hookC');
+require('hookD');
 require('revert');
 
 require('./foo');
