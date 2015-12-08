0d00bed47f20639af312947d5ff796f49a5b5344
Ari Porad <git@ariporad.com>
2015-12-07 19:48:03 -0800

    chore: switch tests to AVA

--- test/test.js
+++ test/test.js
@@ -1,24 +1,18 @@
 /* (c) 2015 Ari Porad (@ariporad) <http://ariporad.com>. License: ariporad.mit-license.org */
-var path = require('path');
-var fork = require('child_process').fork;
+import test from 'ava';
+import path from 'path';
+import { fork } from 'child_process';
 
-
-it('should work', function itTheyShouldWork(done) {
-  var data = '';
-  var child = fork(path.resolve(__dirname, 'fixture', 'main'), {
-    silent: true,
-  });
-  child.on('error', done);
+test(t => {
+  let data = '';
+  const child = fork(path.resolve(__dirname, 'fixture', 'main'), { silent: true });
+  child.on('error', t.fail.bind(t));
   child.stdout.setEncoding('utf8');
   child.stderr.setEncoding('utf8');
-  child.stderr.on('data', function onErrorData(chunk) {
-    console.error('Child Error:', chunk);
-  });
-  child.stdout.on('data', function onData(chunk) {
-    data += chunk;
-  });
-  child.stdout.on('end', function onEnd() {
-    expect(data.trim()).to.equal([
+  child.stderr.on('data', chunk => console.error('Child Error:', chunk));
+  child.stdout.on('data', chunk => data += chunk);
+  child.stdout.on('end', () => {
+    t.is(data.trim(), [
       'foo',
       'in macroA',
       'in macroB',
@@ -28,6 +22,6 @@ it('should work', function itTheyShouldWork(done) {
       'bar',
       'No injectedVar: OK',
     ].join('\n'));
-    done();
+    t.pass();
   });
 });
