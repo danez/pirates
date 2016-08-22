ce510f1c2c451b2ca6ca28b9ae760df30fca3dcc
Daniel Tschinder <code@tschinder.de>
2016-08-22 11:33:23 +0200

    Correctly revert _compile after resolving finished Correctly try reverting handler if no other hook was applied afterwards. Only replace _compile if we know that we should compile

--- test/basics.js
+++ test/basics.js
@@ -1,5 +1,6 @@
 /* (c) 2015 Ari Porad (@ariporad) <http://ariporad.com>. License: ariporad.mit-license.org */
 import test from 'ava';
+import Module from 'module';
 import rewire from 'rewire';
 import { assertModule } from './_utils';
 
@@ -19,6 +20,18 @@ test('basics', t => {
   assertModule(t, 'basics-bar.js', 'in basics-bar \<a\> \<b\>');
 
   reverts.forEach(call);
+});
+
+test('correctly reverts if no other hook', t => {
+  const originalJSLoader = Module._extensions['.js'];
+  const reverts = [
+    t.context.addHook(code => code),
+    t.context.addHook(code => code),
+  ];
+
+  reverts.reverse().forEach(call);
+
+  t.is(originalJSLoader, Module._extensions['.js']);
 });
 
 test('matchers', t => {
