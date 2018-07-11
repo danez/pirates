3485f71d18d361f488239264378bcfc3e49424f9
Daniel Tschinder <daniel@tschinder.de>
2018-07-11 14:49:41 +0200

    chore(package): Update dependencies

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ const Module = module.constructor.length > 1
   ? module.constructor
   : BuiltinModule;
 
-const HOOK_RETURNED_NOTHING_ERROR_MESSAGE = '[Pirates] A hook returned a non-string, or nothing at all! This is a' +
-                                          ' violation of intergalactic law!\n' +
-                                          '--------------------\n' +
-                                          'If you have no idea what this means or what Pirates is, let me explain: ' +
-                                          'Pirates is a module that makes is easy to implement require hooks. One of' +
-                                          ' the require hooks you\'re using uses it. One of these require hooks' +
-                                          ' didn\'t return anything from it\'s handler, so we don\'t know what to' +
-                                          ' do. You might want to debug this.';
+const HOOK_RETURNED_NOTHING_ERROR_MESSAGE = '[Pirates] A hook returned a non-string, or nothing at all! This is a'
+                                          + ' violation of intergalactic law!\n'
+                                          + '--------------------\n'
+                                          + 'If you have no idea what this means or what Pirates is, let me explain: '
+                                          + 'Pirates is a module that makes is easy to implement require hooks. One of'
+                                          + ' the require hooks you\'re using uses it. One of these require hooks'
+                                          + ' didn\'t return anything from it\'s handler, so we don\'t know what to'
+                                          + ' do. You might want to debug this.';
 
 function shouldCompile(filename, exts, matcher, ignoreNodeModules) {
   if (typeof filename !== 'string') return false;
